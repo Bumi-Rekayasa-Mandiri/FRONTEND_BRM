aa13817cbf2188c0ad3f01a5523d51495dc3aed5
mahesgya <anargya115@gmail.com>
2026-03-13 22:23:57 +0800

    feat(home): edit padding top

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,18 +1,15 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
 import FloatingPopup from '../components/common/FloatingPopup';
 import ScrollToTop from '../components/common/ScrollToTop';
 
 const MainLayout = () => {
-  const location = useLocation();
-  const isHome = location.pathname === '/';
-
   return (
     <div className="flex flex-col min-h-screen">
       <ScrollToTop />
       <Navbar />
-      <main className={`grow ${isHome ? '' : 'pt-[72px] md:pt-[112px]'}`}>
+      <main className={`grow`}>
         <Outlet />
       </main>
       <Footer />
